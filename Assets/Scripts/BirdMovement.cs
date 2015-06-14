06771eac7a222c9f162993696b3d1608fe91f961
shiqi shao <shaoshiqi1992@hotmail.com>
2015-06-14 10:06:13 +0800

    The bird move
    The bird now can move,  so does the camera.

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+	Vector3 velocity = Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 flapvelocity;
+	public float forwardspeed;
+	public float maxspeed;
+	bool flap = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			flap = true;
+
+
+		}
+	}
+
+	void FixedUpdate(){
+		velocity.x = forwardspeed;
+		velocity += gravity * Time.deltaTime;
+		if (flap) {
+			flap =false;
+			if(velocity.y<0){
+				velocity.y = 0;
+			}
+			velocity += flapvelocity;
+		}
+		velocity = Vector3.ClampMagnitude (velocity, maxspeed);
+		transform.position += velocity * Time.deltaTime;
+		float angle = 0;
+		if (velocity.y < 0) {
+			angle = Mathf.LerpAngle(0,-90,-velocity.y/maxspeed);
+		}
+		transform.rotation = Quaternion.Euler (0, 0, angle);
+	}
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	float offsetx;
+	Transform player;
+	// Use this for initialization
+	void Start () {
+		GameObject playerstart = GameObject.FindGameObjectWithTag("Player");
+		if (playerstart == null) {
+			Debug.LogError("Cant find the bird");
+			return;
+		}
+		player = playerstart.transform;
+		offsetx = this.transform.position.x - player.position.x;
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null){
+			Vector3 position = transform.position;
+			position.x = player.position.x + offsetx;
+			transform.position = position ;
+		}
+	}
+}
